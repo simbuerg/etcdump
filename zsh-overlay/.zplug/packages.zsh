8b97d0d58d01fe610aab323f3d2beba551733a49
Andreas Simbuerger <simbuerg@fim.uni-passau.de>
2023-07-25 16:03:42 +0200

    update: zsh-overlay

--- zsh-overlay/.zplug/packages.zsh
+++ zsh-overlay/.zplug/packages.zsh
@@ -1,10 +1,7 @@
-zplug 'zplug/zplug', hook-build:'zplug --self-manage'
-zplug "simbuerg/3fa2cf910d40b96a84e36affc361254a", from:gist, use:bindkeys.zsh
 zplug "mafredri/zsh-async"
 zplug "sindresorhus/pure", use:pure.zsh, as:theme
 zplug "zsh-users/zsh-autosuggestions", use:zsh-autosuggestions.zsh, defer:3
 zplug "zsh-users/zsh-syntax-highlighting", use:zsh-syntax-highlighting.zsh
 zplug "willghatch/zsh-saneopt", use:saneopt.plugin.zsh
-zplug "/usr/lib/ruby/gems/2.7.0/gems/tmuxinator-2.0.1/completion", use:tmuxinator.zsh, from:local
 zplug "/usr/share/fzf", use:completion.zsh, from:local
 zplug "/usr/bin", use:virtualenvwrapper.sh, from:local
